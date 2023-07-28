bd12cdcf91256101476ba393e4720e51f66037f0
Gengshan Yang <luanyingjian@gmail.com>
2023-07-28 16:43:06 -0400

    update

--- scripts/run_rendering_parallel.py
+++ scripts/run_rendering_parallel.py
@@ -1,4 +1,5 @@
 # WIP by Gengshan Yang
+# generate three visualizations (reference view, bev, turntable) rendering in parallel
 # python scripts/run_rendering_parallel.py logdir/dog-98-category-comp/opts.log 0-2 0,1,2
 import sys
 import subprocess
@@ -8,31 +9,38 @@ flagfile = sys.argv[1]
 
 # Set the range of inst_ids.
 start_inst_id, end_inst_id = map(int, sys.argv[2].split("-"))
+id_list = list(range(start_inst_id, end_inst_id + 1))
 
 # Set the devices id
 dev_list = sys.argv[3].split(",")
 dev_list = list(map(int, dev_list))
 num_devices = len(dev_list)
+id_per_device = len(id_list) // num_devices + 1
 
 print(
-    "rendering from inst_id",
-    start_inst_id,
-    "to",
-    end_inst_id,
+    "rendering videos",
+    id_list,
     "on devices",
     dev_list,
 )
 
 # Loop over each device.
-for device in dev_list:
+for dev_id, device in enumerate(dev_list):
     # Initialize an empty command list for this device.
     command_for_device = []
 
     # Loop over the inst_ids assigned to this device.
-    for inst_id in range(device, end_inst_id + 1, num_devices):
+    assigned_ids = id_list[dev_id * id_per_device : (dev_id + 1) * id_per_device]
+    for inst_id in assigned_ids:
         # Add the command for this inst_id to the device's command list.
         command_for_device.append(
             f"CUDA_VISIBLE_DEVICES={device} python lab4d/render.py --flagfile={flagfile} --load_suffix latest --inst_id {inst_id} --render_res 256"
+        )
+        command_for_device.append(
+            f"CUDA_VISIBLE_DEVICES={device} python lab4d/render.py --flagfile={flagfile} --load_suffix latest --inst_id {inst_id} --render_res 256 --viewpoint rot-0-360"
+        )
+        command_for_device.append(
+            f"CUDA_VISIBLE_DEVICES={device} python lab4d/render.py --flagfile={flagfile} --load_suffix latest --inst_id {inst_id} --render_res 256 --viewpoint bev-90"
         )
 
         # Add a delay between commands to avoid overloading the device.
@@ -43,5 +51,6 @@ for device in dev_list:
 
     # Start a screen session for this device, executing the device's command string.
     subprocess.Popen(
-        f'screen -S render-{device} -d -m bash -c "{command_str}"', shell=True
+        f'screen -S render-{device}-{assigned_ids} -d -m bash -c "{command_str}"',
+        shell=True,
     )
