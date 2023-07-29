09904930ee263be49551b6e99a2e5159fd02950c
Gengshan Yang <luanyingjian@gmail.com>
2023-07-29 17:36:34 -0400

    update render parallel

--- scripts/run_rendering_parallel.py
+++ scripts/run_rendering_parallel.py
@@ -1,5 +1,5 @@
 # WIP by Gengshan Yang
-# generate three visualizations (reference view, bev, turntable) rendering in parallel
+# generate three visualizations (reference view, bev, turntable) rendering, mesh export in parallel
 # python scripts/run_rendering_parallel.py logdir/dog-98-category-comp/opts.log 0-2 0,1,2
 import sys
 import subprocess
@@ -24,6 +24,12 @@ print(
     dev_list,
 )
 
+# render proxy over rounds
+logdir = flagfile.rsplit("/", 1)[0]
+subprocess.Popen(
+    f"python scripts/render_intermediate.py --testdir {logdir}/", shell=True
+)
+
 # Loop over each device.
 for dev_id, device in enumerate(dev_list):
     # Initialize an empty command list for this device.
@@ -41,6 +47,9 @@ for dev_id, device in enumerate(dev_list):
         )
         command_for_device.append(
             f"CUDA_VISIBLE_DEVICES={device} python lab4d/render.py --flagfile={flagfile} --load_suffix latest --inst_id {inst_id} --render_res 256 --viewpoint bev-90"
+        )
+        command_for_device.append(
+            f"CUDA_VISIBLE_DEVICES={device} python lab4d/export.py --flagfile={flagfile} --load_suffix latest --inst_id {inst_id}"
         )
 
         # Add a delay between commands to avoid overloading the device.
