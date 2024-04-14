0522ef27651258cd15ae4a83c8a0ea1c43ab675c
Gengshan Yang <luanyingjian@gmail.com>
2024-04-14 15:32:27 -0400

    update rotation orghoganalization; new data

--- lab4d/utils/quat_transform.py
+++ lab4d/utils/quat_transform.py
@@ -24,6 +24,25 @@ QuaternionTranslation = Tuple[torch.Tensor, torch.Tensor]
 """
 
 
+def symmetric_orthogonalization(x):
+    """Maps 9D input vectors onto SO(3) via symmetric orthogonalization.
+
+    x: should have size [batch_size, 9]
+
+    Output has size [batch_size, 3, 3], where each inner 3x3 matrix is in SO(3).
+    """
+    shape = x.shape
+    m = x.view(-1, 3, 3)
+    u, s, v = torch.svd(m)
+    vt = torch.transpose(v, 1, 2)
+    det = torch.det(torch.matmul(u, vt))
+    det = det.view(-1, 1, 1)
+    vt = torch.cat((vt[:, :2, :], vt[:, -1:, :] * det), 1)
+    r = torch.matmul(u, vt)
+    r = r.reshape(shape)
+    return r
+
+
 # @torch.jit.script
 def _quaternion_conjugate(q: torch.Tensor) -> torch.Tensor:
     """
