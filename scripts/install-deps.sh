7e0c282cfc0640dfef657988d9391edfae364b4b
Gengshan Yang <luanyingjian@gmail.com>
2024-07-03 12:57:55 -0400

    Update download link in install-deps.sh

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -2,7 +2,7 @@ mim install mmcv
 
 (cd lab4d/third_party/quaternion && CUDA_HOME=$CONDA_PREFIX pip install .)
 
-mkdir ./preprocess/third_party/Track-Anything/checkpoints; gdown --fuzzy https://drive.google.com/uc?id=10wGdKSUOie0XmCr8SQ2A2FeDe-mfn5w3 -O ./preprocess/third_party/Track-Anything/checkpoints/E2FGVI-HQ-CVPR22.pth
+mkdir ./preprocess/third_party/Track-Anything/checkpoints; wget "https://www.dropbox.com/scl/fi/o86gx6zn27b494m937n2i/E2FGVI-HQ-CVPR22.pth?rlkey=j15ue65ryy8jb1mvn2htf0jtk&st=t4zyl5jk&dl=0" -O ./preprocess/third_party/Track-Anything/checkpoints/E2FGVI-HQ-CVPR22.pth
 
 wget https://www.dropbox.com/s/bgsodsnnbxdoza3/vcn_rob.pth -O ./preprocess/third_party/vcnplus/vcn_rob.pth
 
