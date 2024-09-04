b8716a9b1032874672389f8732bcf05fa292a626
Gengshan Yang <luanyingjian@gmail.com>
2024-09-03 21:15:03 -0700

    update scripts

---
+++ projects/csim/run_multi_cat.sh
@@ -0,0 +1,8 @@
+envname=Oct5at10-49AM-poly-cp
+seqname=$1
+dev=$2
+
+# python projects/csim/rerun_dinov2.py $seqname $dev
+# bash projects/csim/recon_scene.sh $envname $dev
+bash projects/csim/align_scene_multi.sh $envname $seqname $dev
+bash projects/csim/recon_compose_multi.sh $seqname urdf-quad $dev # cat, with shape
