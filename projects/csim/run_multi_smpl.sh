0b179c5239f9d8621cc40c07aeacaca120308842
Gengshan Yang <luanyingjian@gmail.com>
2024-09-07 00:50:08 -0400

    del

--- projects/csim/run_multi_smpl.sh
+++
@@ -1,9 +0,0 @@
-#envname=Oct5at10-49AM-poly
-envname=$1
-seqname=$2
-dev=$3
-
-python projects/csim/rerun_dinov2.py $seqname $dev
-bash projects/csim/recon_scene.sh $envname $dev
-bash projects/csim/align_scene_multi.sh $envname $seqname $dev
-bash projects/csim/recon_compose_multi_smpl.sh $seqname $dev # human
