3d859d160ec198b2c22aa6cea24ddddb3a0006d7
chaoyang <cwang9@snapchat.com>
2025-02-01 23:48:10 -0800

    update copyright, tidy quaternion code

--- lab4d/third_party/quaternion/__init__.py
+++ lab4d/third_party/quaternion/__init__.py
@@ -1,2 +1,3 @@
+## Copyright (c) 2023 Chaoyang Wang, Carnegie Mellon University.
 from .mat3x3 import mat3x3_inv
 from .quaternion import quaternion_conjugate, quaternion_mul
--- lab4d/third_party/quaternion/src/matinv.cu
+++ lab4d/third_party/quaternion/src/matinv.cu
@@ -1,3 +1,4 @@
+// Copyright (c) 2023 Chaoyang Wang, Carnegie Mellon University.
 #include <stdint.h>
 
 #include <cuda.h>
